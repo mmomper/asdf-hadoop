bd28bdb49da6c18ac49a1de18022b2f39914cbb3
Moritz Momper <moritz.momper@adesso.de>
2024-05-28 18:22:22 +0200

    first try

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for hadoop.
-GH_REPO="https://archive.apache.org/dist/hadoop/common/"
+HADOOP_ARCHIVE_URL="https://archive.apache.org/dist/hadoop/common"
 TOOL_NAME="hadoop"
 TOOL_TEST="hadoop --help"
 
@@ -14,26 +13,21 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if hadoop is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if hadoop has other means of determining installable versions.
-	list_github_tags
+	local archive_src="${1:-}"
+
+	if [[ -z "${archive_src}" ]]; then
+		echo ""
+		return
+	fi
+
+	{ grep -Eo '<a href="hadoop-[0-9]+\.[0-9]+\.[0-9]+(\-[a-z0-9]+)?\/">' <<<"${archive_src}" || :; } |
+		sed -E 's/^<a href="hadoop-(.+)\/">$/\1/g'
 }
 
 download_release() {
@@ -41,8 +35,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for hadoop
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$HADOOP_ARCHIVE_URL/hadoop-${version}/hadoop-${version}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -51,7 +44,7 @@ download_release() {
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="$3"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -61,10 +54,9 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert hadoop executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
